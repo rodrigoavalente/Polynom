6c843698b7b0f48ace57d81ab018464bc0e33b42
Rodrigo Valente <rodrigoavrav@gmail.com>
2014-07-24 10:29:18 -0300

    Adicionado comentários para o doxygen

--- polynom.h
+++ polynom.h
@@ -1,3 +1,9 @@
+//! Classe Polinômio.
+
+/*! 
+    Classe criado com a finalidade de tratar expressões numéricas de uma única varíavel.
+*/
+
 #ifndef POLYNOM_H
 #define POLYNOM_H
 #include <cstdlib>
@@ -27,12 +33,51 @@ private:
 
 
 public:
+    //! Construtor padrão da classe.
+    /*! 
+        Inicia a variável com 's'.
+    */
     Polynom();
+
+    //! Construtor que inicia o numerador e o denominador do Polinômio.
+    /*! 
+       Inicia o polinômio de acordo com a quantidade de termos no numerador e denominador.
+       \param Num um argumento inteiro que representa a quantidade de termos no numerador.
+       \param Den um argumento inteiro que representa a quantidade de termos no denominador.
+    */
     Polynom(int Num, int Den);
+
+    //! Construdor para um polinômio que só tenha um numerador.
+    /*!
+       Inicia o polinômio com a quantidade de termos no numerador, sempre considera o denominador como 1.
+       \param Num um argumento inteiro que representa a quantidade de termos no numerador.
+    */
     Polynom(int Num);
+    
+    //! Construdor de cópia.
+    /*!
+       Construtor responsável para se realizar cópias dos endereços de memórias enquanto se opera com polinômios.
+       \param CopyPolynom uma constante do tipo de Polynom que referência o polinôio a ser copiado.
+    */
     Polynom(const Polynom<TypeOfClass> &CopyPolynom);
+    
+    //! Construtor que inicia um polinômio com uma entrada do tipo string, sempre considera o denominador como 1.
+    /*!
+       \param Num um argumento do tipo string que inicializa o numerador polinômio de acordo com o texto de entrada.
+    */
     Polynom(std::string Num);
+    
+    //! Construtor que inicia um polinômio com uma entrada do tipo string.
+    /*!
+       \param Num um argumento do tipo string que incializa o numerador do polinômio de acordo com o texto de entrada.
+       \param Den um argumento do tipo string que incializa o denominador do polinômio de acordo com o texto de entrada.
+    */
     Polynom(std::string Num, std::string Den);
+    
+    //! Destrutor padrão da classe.
+    /*!
+       Libera toda a memória aloca utilzada por objetos da classe.
+    */
     ~Polynom();
 
     Polynom<TypeOfClass> operator+(Polynom<TypeOfClass> P);
